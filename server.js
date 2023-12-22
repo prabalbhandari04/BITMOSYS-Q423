dc73e5540c3598ff4ef9fa0feeb2fe7401348d74
Prabal Bhandari <54536906+prabalbhandari04@users.noreply.github.com>
2023-12-22 19:03:44 +0530

    fix : cors

--- server.js
+++ server.js
@@ -6,21 +6,29 @@ const cryptoRoutes = require('./routes/cryptoCoin.route');
 const walletRoutes = require('./routes/wallet.route');
 const app = express();
 
+// Enable CORS for all origins
+app.use((req, res, next) => {
+res.header('Access-Control-Allow-Origin', '*');
+res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+next();
+});
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+useNewUrlParser: true,
+useUnifiedTopology: true,
 });
 
 // Access the connection object and handle events
 const db = mongoose.connection;
 
 db.on('error', (error) => {
-  console.error('MongoDB connection error:', error);
+console.error('MongoDB connection error:', error);
 });
 
 db.once('open', () => {
-  console.log('Server connected to database.');
+console.log('Server connected to the database.');
 });
 
 // Middleware
@@ -32,13 +40,12 @@ app.use('/api/v1/wallet', walletRoutes);
 
 // New route to check if the server is running in production
 app.get('/', (req, res) => {
-  const environment = process.env.NODE_ENV || 'development';
-  res.json({ message: `Server running on ${environment} environment` });
+const environment = process.env.NODE_ENV || 'development';
+res.json({ message: `Server running on ${environment} environment` });
 });
-
 
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+console.log(`Server is running on port ${PORT}`);
 });
